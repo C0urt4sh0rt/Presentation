c992842f9ebba16722410ef78ff192cbc19152e2
Courtney Hampton <jaye0888@gmail.com>
2022-10-18 19:12:55 +0200

    Updating Css

--- projects.js
+++ projects.js
@@ -1,8 +1,12 @@
-var html = document.getElementsByClassName("topbutton");
-html.click(function () {
-  if ($("topbutton").hasClass("active")) {
-    $("topbutton").removeClass("active");
-  } else {
-    $("topbutton").addClass("active");
+const htmlButton = document.getElementById("html");
+const cssButton = document.getElementById("css");
+const jsButton = document.getElementById("js");
+const allProjectsButtons = document.querySelectorAll(".individual");
+
+const highlightHtml = function (event) {
+  for (let i = 0; i < allProjectsButtons.length; i++) {
+    allProjectsButtons[i].classList.toggle("highlight");
   }
-});
+};
+
+htmlButton.addEventListener("click", highlightHtml);
